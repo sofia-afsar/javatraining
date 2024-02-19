ffd6cb9107edba3282c990ed0ed7928b9e3ab55d
Sofiaafsar <115290901+Sofiaafsar@users.noreply.github.com>
2024-02-19 15:27:39 +0530

    Update Foodapp.java

--- Day1/Foodapp.java
+++ Day1/Foodapp.java
@@ -1,1 +1,44 @@
+package javatraining;
+import java.util.*;
+public class foodapp{
+    public static void main(String[]args) {
+        int flag = 0;
+        while (flag == 0) {
+            System.out.println("-------Hotel Taj ------\n" +
+                    "1.\tCAKES (2USD)\n" +
+                    "2.\tCOOLDRINK (1USD)\n" +
+                    "3\tSNACKS (3USD)\n" +
+                    "4\tCURRIES (4USD)\n" +
+                    "5/EXIT");
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the ItemCode ");
+            int itemcode = sc.nextInt();
+            System.out.println("Enter the no of packs");
+            int packs = sc.nextInt();
+
+            switch (itemcode) {
+                case (1):
+                    System.out.println("the amount to be paid is " + packs * 2);
+                    break;
+                case (2):
+                    System.out.println("the amount to be paid is " + packs);
+                    break;
+                case (3):
+                    System.out.println("the amount to be paid is " + packs * 3);
+                    break;
+                case (4):
+                    System.out.println("the amount to be paid is " + packs * 4);
+                    break;
+                case (5):
+                    System.out.println("EXIT");
+                    flag=1;
+                    break;
+                default:
+                    System.out.println("again re enter the correct itemcode between 1 and 5");
+            }
+        }
+    }
+
+}
 
