07399d108d0c2ce80969b2291aa9c7a052de611a
Sofiaafsar <115290901+Sofiaafsar@users.noreply.github.com>
2024-02-19 14:17:01 +0530

    Update compare2integers.java

--- Day1/compare2integers.java
+++ Day1/compare2integers.java
@@ -1,1 +1,20 @@
+package javatraining;
+import java.util.*;
+public class Compare2integers {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+        int num1=sc.nextInt();
+        System.out.println("Enter second number ");
+        int num2=sc.nextInt();
+        if(num1>num2){
+            System.out.println(num1+" greater than "+num2);
+        } else if (num1<num2) {
+            System.out.println(num1+" less than "+num2);
+        }
+        else{
+            System.out.println(num1+" equal to "+num2);
+        }
+    }
+}
 
